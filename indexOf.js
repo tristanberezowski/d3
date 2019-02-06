de6d45be25ee58be240a7a445e53dee0b90a0bbe
Tristan Berezowski <tristanberezowski@gmail.com>
2019-02-06 20:04:53 +0000

    update indexOf.js

--- indexOf.js
+++ indexOf.js
@@ -8,8 +8,15 @@ var stringDetails = {};
 for (var i = 0;i < string.length;i++)
 {
   if (stringDetails[string[i]] === undefined)
-    stringDetails[string[i]] = [];
-  stringDetails[string[i]].push(i);
+  {
+    stringDetails[string[i]] = {};
+    stringDetails[string[i]].indices = [];
+    stringDetails[string[i]].count = 0;
+
+  }
+  stringDetails[string[i]].indices.push(i);
+  stringDetails[string[i]].count++;
+
 }
 delete stringDetails[' '];
 return stringDetails;
